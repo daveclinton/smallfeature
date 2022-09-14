01d7eb4ee329c74ff953f60b26a38fa8d75ea511
daveclinton <clintondavid46@gmail.com>
2022-09-14 22:45:47 +0300

    refactor: change navbar style

--- pages/components/navbar/index.tsx
+++ pages/components/navbar/index.tsx
@@ -1,97 +1,53 @@
-import { ReactNode } from "react";
-import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { Flex, Link, ListItem, UnorderedList } from "@chakra-ui/react";
+import React from "react";
 
-const Links = ["Bio", "Projects", "Contact"];
-
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={"md"}
-    _hover={{
-      textDecoration: "none",
-      bg: useColorModeValue("black", "black"),
-      color: "#ffffff",
-      fontWeight: "bolder",
-    }}
-    href={"#"}
-  >
-    {children}
-  </Link>
-);
-
-export default function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
+function index() {
   return (
-    <>
-      <Box bg="#ffffff" px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <IconButton
-            size={"md"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon color="black" />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={100}>
-            <Flex alignItems={"center"}>
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rounded={"full"}
-                  variant={"link"}
-                  cursor={"pointer"}
-                  minW={0}
-                >
-                  <Avatar size={"sm"} />
-                </MenuButton>
-                <MenuList>
-                  <MenuItem>Github Profile</MenuItem>
-                  <MenuItem>Resume</MenuItem>
-                </MenuList>
-              </Menu>
-            </Flex>
-            <HStack
-              as={"nav"}
-              spacing={10}
-              display={{ base: "none", md: "flex" }}
-              color="#000000"
-              fontWeight="bolder"
-            >
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
-          </HStack>
-        </Flex>
-
-        {isOpen ? (
-          <Box pb={4} display={{ md: "none" }} color="#000000" bg="#ffffff">
-            <Stack as={"nav"} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </>
+    <Flex
+      bg="black"
+      h="80px"
+      justifyContent="center"
+      alignItems="center"
+      fontSize="1.2rem"
+      pos="sticky"
+      top={0}
+      zIndex={20}
+    >
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        w="100%"
+        h="80px"
+        maxW="1500px"
+      >
+        <Link
+          color="white"
+          alignItems="center"
+          ml="20px"
+          cursor="pointer"
+          textDecoration="none"
+          fontSize="2rem"
+          flexGrow={1}
+        >
+          David
+        </Link>
+        <UnorderedList>
+          <ListItem>
+            <Link>Home</Link>
+          </ListItem>
+          <ListItem>
+            <Link>About</Link>
+          </ListItem>
+          <ListItem>
+            <Link>Blog</Link>
+          </ListItem>
+          <ListItem>
+            <Link>Contact Me</Link>
+          </ListItem>
+        </UnorderedList>
+      </Flex>
+    </Flex>
   );
 }
+
+export default index;
