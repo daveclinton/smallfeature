bb68b0569c104ba3772e21d990b6d601d8e5df2c
daveclinton <clintondavid46@gmail.com>
2023-06-18 22:38:25 +0300

    feat: Add color mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { Box, Text } from "@chakra-ui/react";
+// import { Button, useColorMode } from "@chakra-ui/react";
 
-function App() {
+import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Flex, Text } from "@chakra-ui/layout";
+import { Avatar, IconButton, useColorMode } from "@chakra-ui/react";
+
+const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box>
-      <Text>Hello</Text>
-    </Box>
+    <>
+      <Flex m="20px" align="center" justify="space-between">
+        <Flex align="center" justify="space-between">
+          <Avatar mr="15px" name="David Clinton" />
+          <Text>David C</Text>
+        </Flex>
+        <Flex align="center">
+          <IconButton
+            variant="unstyled"
+            aria-label=""
+            onClick={toggleColorMode}
+            icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+          />
+          <HamburgerIcon />
+        </Flex>
+      </Flex>
+    </>
   );
-}
+};
 
 export default App;
--- theme.ts
+++ theme.ts
@@ -1,17 +1,17 @@
 import { extendTheme } from "@chakra-ui/react";
+export const colors = {
+  dark: "#171923",
+  light: "#fff",
+};
 
 const theme = extendTheme({
+  colors,
   config: {
     initialColorMode: "light",
     useSystemColorMode: false,
   },
   styles: {
-    global: {
-      body: {
-        bg: "#171923",
-        color: "#fff",
-      },
-    },
+    global: {},
   },
 });
 
