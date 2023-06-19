33e1d77424ab0e2a31d386deafbfb0c3744d37ee
daveclinton <clintondavid46@gmail.com>
2023-06-19 20:52:13 +0300

    feat: Add hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Center,
 } from "@chakra-ui/react";
 import Logo from "../assets/david.svg";
 
@@ -16,41 +17,45 @@ const Navbar: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Flex
-      m={{ base: "20px", lg: "20px 180px 0px 180px" }}
-      align="center"
-      justify="space-between"
-    >
-      <Image src={Logo} h="30px" />
-      <Flex gap="20px" align="center">
-        <IconButton
-          variant="unstyled"
-          aria-label=""
-          onClick={toggleColorMode}
-          icon={
-            colorMode === "light" ? (
-              <MoonIcon boxSize="20px" />
-            ) : (
-              <SunIcon boxSize="20px" />
-            )
-          }
-        />
-        <Menu>
-          <MenuButton
-            as={IconButton}
-            aria-label="Options"
-            icon={<HamburgerIcon color="classicGreen" boxSize="30px" />}
+    <Center>
+      <Flex
+        w="100%"
+        m="20px"
+        maxW="1240px"
+        align="center"
+        justify="space-between"
+      >
+        <Image src={Logo} h="30px" />
+        <Flex gap="20px" align="center">
+          <IconButton
             variant="unstyled"
+            aria-label=""
+            onClick={toggleColorMode}
+            icon={
+              colorMode === "light" ? (
+                <MoonIcon boxSize="20px" />
+              ) : (
+                <SunIcon boxSize="20px" />
+              )
+            }
           />
-          <MenuList>
-            <MenuItem>Home</MenuItem>
-            <MenuItem>Projects</MenuItem>
-            <MenuItem>Blog</MenuItem>
-            <MenuItem>Paid Articles</MenuItem>
-          </MenuList>
-        </Menu>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon color="classicGreen" boxSize="30px" />}
+              variant="unstyled"
+            />
+            <MenuList>
+              <MenuItem>Home</MenuItem>
+              <MenuItem>Projects</MenuItem>
+              <MenuItem>Blog</MenuItem>
+              <MenuItem>Paid Articles</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
       </Flex>
-    </Flex>
+    </Center>
   );
 };
 
