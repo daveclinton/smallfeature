11b9d75eae87e3bb4e51bfd162cb10a3550ef6ac
daveclinton <clintondavid46@gmail.com>
2023-06-19 19:44:06 +0300

    feat: Add themes

--- theme.ts
+++ theme.ts
@@ -16,6 +16,20 @@ const theme = extendTheme({
     global: {},
   },
   components: {},
+  textStyles: {
+    h1: {
+      fontSize: "calc(1.8rem + 3vmin)",
+      lineHeight: 1.2,
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: "calc(1.1rem + .5vmin)",
+      lineHeight: 1.4,
+      opacity: 0.8,
+      fontWeight: 400,
+    },
+    p: { fontSize: "1rem", fontWeight: 400 },
+  },
 });
 
 export default theme;
