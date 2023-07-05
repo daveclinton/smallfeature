a672f158c5ea606b6752f11f7b2771e09e8fb06e
daveclinton <clintondavid46@gmail.com>
2023-07-05 14:56:35 +0300

    fix: Fix height of card

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,33 +1,56 @@
 import * as React from "react";
-import { Center, LinkBox, Text, Image } from "@chakra-ui/react";
+import {
+  Center,
+  LinkBox,
+  Text,
+  Image,
+  Flex,
+  CircularProgress,
+  Heading,
+  Box,
+} from "@chakra-ui/react";
 import { usePosts } from "../query/posts";
 
 const ArticleCard: React.FC = () => {
-  const { data } = usePosts();
+  const { isLoading, data } = usePosts();
   return (
     <Center display="flex" gap="10px" flexWrap="wrap" m="20px">
-      {data?.map((post) => (
-        <LinkBox
-          key={post.id}
-          as="article"
-          maxW="sm"
-          p="5"
-          borderWidth="1px"
-          rounded="md"
-        >
-          <Image
-            src="https://itsfoss.com/content/images/size/w600/format/webp/2023/07/birthday-wish-linux-terminal.png"
-            alt="Dan Abramov"
-          />
-          <Text mt="15px" fontSize="1.4rem">
-            {post.attributes.title}
-          </Text>
-          <Text opacity="0.8" fontSize="1rem">
-            Make the special occasion for your loved ones even more special by
-            creating an ASCII birthday animation in the Linux terminal.
-          </Text>
-        </LinkBox>
-      ))}
+      {isLoading ? (
+        <Flex mt="50px" justify="center" align="center" flexDir="column">
+          <CircularProgress value={80} />
+          <Heading>Fetching articles</Heading>
+        </Flex>
+      ) : (
+        <>
+          {data?.map((post) => (
+            <LinkBox
+              key={post.id}
+              as="article"
+              maxW="sm"
+              borderWidth="1px"
+              rounded="md"
+              display="flex"
+              flexDir="column"
+            >
+              <Image
+                objectFit="cover"
+                src="https://itsfoss.com/content/images/size/w600/format/webp/2023/07/birthday-wish-linux-terminal.png"
+                alt="Dan Abramov"
+              />
+              <Box p="20px">
+                <Text h="80px" mt="15px" fontSize="1.4rem">
+                  {post.attributes.title}
+                </Text>
+                <Text h="80px" opacity="0.8" fontSize="1rem">
+                  Make the special occasion for your loved ones even more
+                  special by creating an ASCII birthday animation in the Linux
+                  terminal.
+                </Text>
+              </Box>
+            </LinkBox>
+          ))}
+        </>
+      )}
     </Center>
   );
 };
