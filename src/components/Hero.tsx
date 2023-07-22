3c274671f0208f7aaae01abbbf377058583518ad
daveclinton <clintondavid46@gmail.com>
2023-07-22 10:37:29 +0300

    feat: Add profile avatar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,59 @@
 import * as React from "react";
-import { Center, Flex, Highlight, Text } from "@chakra-ui/react";
+import { Center, Flex, Highlight, Text, Image } from "@chakra-ui/react";
+import authorImage from "../assets/me.svg";
 
 const Hero: React.FC = () => {
   return (
-    <Center>
-      <Flex m="20px" maxW="1240px" display="flex" flexDir="column" w="100%">
-        <Text textAlign="center" mb="1rem" textStyle="h1">
-          <Highlight
-            query={["David Clinton"]}
-            styles={{ color: "classicGreen" }}
+    <Center p={{ lg: "20px" }}>
+      <Flex
+        justify="space-between"
+        mt="20px"
+        mb="20px"
+        maxW="1240px"
+        display="flex"
+        w="100%"
+        flexDir={{ base: "column-reverse", lg: "row" }}
+      >
+        <Flex flexDir="column" m="auto" p="10px">
+          <Text
+            textAlign={{ base: "center", lg: "start" }}
+            mb="1rem"
+            textStyle="h1"
           >
-            Hey there, I'm David Clinton
-          </Highlight>
-        </Text>
-        <Text mb="1rem" textStyle="h2">
-          Software and Web Developer | Frontend Developer
-        </Text>
-        <Text textAlign="center" textStyle="h3">
-          Welcome to my digital garden where I share what I'm learning about
-          shipping great products, becoming a better developer and growing a
-          career in tech.
-        </Text>
+            <Highlight
+              query={["David Clinton"]}
+              styles={{ color: "classicGreen" }}
+            >
+              Hey there, I'm David Clinton
+            </Highlight>
+          </Text>
+          <Text
+            textAlign={{ base: "center", lg: "start" }}
+            mb="1rem"
+            textStyle="h2"
+          >
+            Software and Web Developer | Frontend Developer | Technical Author
+          </Text>
+          <Text mb="1rem" textStyle="h3">
+            Welcome to my digital garden where I write about many things,
+            including, but not limited to React, Typescript, Javascript, Chakra
+            UI, many more web dev related topics and Cloud Computing
+          </Text>
+          <Text mb="1rem" textStyle="h3">
+            You can carry on scrolling for a bit more info about me if you like.
+          </Text>
+          <Text mb="1rem" textStyle="h3">
+            Or if you want to get in touch, feel free to reach out to me...
+          </Text>
+        </Flex>
+        <Center>
+          <Image
+            src={authorImage}
+            maxW="24rem"
+            w={{ base: "50%", lg: "auto" }}
+            m="auto"
+          />
+        </Center>
       </Flex>
     </Center>
   );
